a92d6b258950e57c9ee7c104d1fa1366dbc5790b
Tarun Mittal <tm6622@g.rit.edu>
2023-05-03 18:12:43 -0400

    using API users

--- src/App.js
+++ src/App.js
@@ -8,39 +8,36 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [    
-      {
-        name: 'Linda',
-        id: '12e1231e'
-      },
-      {
-        name: 'Frank',
-        id: '12ed2dacas'
-      },
-      {
-        name: 'Jacky',
-        id: '1231e'
-      },
-      {
-        name: 'Andrei',
-        id: '12e1e213'
-      },
-    ], 
+      monsters: [],
   };
 }
 
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    },
+    () => {
+      console.log(this.state);
+    }
+    )
+  );
+}
+
   render() {
-    return <div className="App">
+    return ( 
+    <div className="App">
       {
         this.state.monsters.map((monster) => {
           return ( 
           <div key={monster.id}>
             <h1>{monster.name}</h1>
-            </div>
+          </div>
           );
-        })
-      }
+        })}
     </div>
+    );
   }
 }
   
