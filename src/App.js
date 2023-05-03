ae819651777913385e2db56088a699923dbb2446
Tarun Mittal <tm6622@g.rit.edu>
2023-05-03 18:39:42 -0400

    adding a search box

--- src/App.js
+++ src/App.js
@@ -3,33 +3,40 @@ import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
+  class App extends Component {
+    constructor() {
+      super();
 
-    this.state = {
-      monsters: [],
-  };
-}
+      this.state = {
+        monsters: [],
+    };
+    console.log('constructor');
+  }
 
-componentDidMount() {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => this.setState(() => {
-      return {monsters: users}
-    },
-    () => {
-      console.log(this.state);
-    }
-    )
-  );
-}
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => 
+      this.setState(
+        () => {
+          return {monsters: users}
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+    );
+  }
 
   render() {
+    console.log('render');
     return ( 
     <div className="App">
-      {
-        this.state.monsters.map((monster) => {
+      <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => { 
+        console.log(event.target.value);
+      }} />
+      {this.state.monsters.map((monster) => {
           return ( 
           <div key={monster.id}>
             <h1>{monster.name}</h1>
