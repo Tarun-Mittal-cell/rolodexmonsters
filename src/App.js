79db3bcd13c68e5e5d4f0af5986902fd6866ce9c
Tarun Mittal <tm6622@g.rit.edu>
2023-05-04 23:32:39 -0400

    saving the state

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 
       this.state = {
         monsters: [],
+        searchField: ''
     };
     console.log('constructor');
   }
@@ -31,21 +32,26 @@ import './App.css';
 
   render() {
     console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return ( 
     <div className='App'>
-      <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => { 
-        console.log(event.target.value);
-        const searchString = event.target.value.toLocaleLowerCase();
-        const filteredMonsters = this.state.monsters.filter((monster) => {
-          return monster.name.toLocaleLowerCase().includes(searchString);
-        });
-
-        this.setState(() => {
-          return { monsters: filteredMonsters };
-        });
-      }} 
-    />
-      {this.state.monsters.map((monster) => {
+      <input className='search-box' 
+      type='search' 
+      placeholder='search monsters' 
+      onChange={(event) => { 
+        const searchField = event.target.value.toLocaleLowerCase();
+        this.setState(
+          () => {
+          return { searchField };
+        }
+      );
+    }} 
+  />
+      {filteredMonsters.map((monster) => {
           return ( 
           <div key={monster.id}>
             <h1>{monster.name}</h1>
