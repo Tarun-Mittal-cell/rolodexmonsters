2340c067f1a057aa82b655b3fcdc85d274e57d82
Tarun Mittal <tm6622@g.rit.edu>
2023-05-05 21:00:58 -0400

    add use-state hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
+
+  const [searchField, setSearchField] = useState(''); // [value, setValue]
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  }
+
   return (
     <div className='App'>
       <h1 className='app-title'>Monsters Rolodex</h1>
 
-      
-      {/* 
       <SearchBox
         className='monsters-search-box'
         onChangeHandler={onSearchChange} 
         placeholder='search monsters' 
-    />
+      />
+      
+      {/* 
     <CardList monsters={filteredMonsters}/> */}
   </div>
   );
